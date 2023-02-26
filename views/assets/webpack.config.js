380660855438cafcdde94a299b3ee1b274f10ac3
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-02-26 10:34:02 +0100

    refactored webpack and npm scripts

--- views/assets/webpack.config.js
+++ views/assets/webpack.config.js
@@ -1,12 +1,52 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require('path');
+const fs = require('fs');
 
 const isProduction = process.env.NODE_ENV == 'production';
 
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+
+const copyImages = () => {
+    if (fs.existsSync(`../../public/images`)) {
+        fs.rmSync(`../../public/images/*`, { force: true });
+    }
+    else {
+        fs.mkdirSync(`../../public/images`);
+    }
+
+    fs.readdirSync(`./images`).forEach(file => {
+        fs.copyFileSync('./images/' + file, '../../public/images/' + file);
+    });
+
+    console.log("Images copied")
+};
+
+const createIcons = () => {
+    if (fs.existsSync(`../../public/icons`)) {
+        fs.rmSync(`../../public/icons/*`, { force: true });
+    }
+    else {
+        fs.mkdirSync(`../../public/icons`);
+    }
+
+    const filelist = [];
+
+    fs.readdirSync(`./icons`).forEach(file => {
+        filelist.push(file.split('.').slice(0, -1).join('.'));
+        fs.copyFileSync('./icons/' + file, '../../public/icons/' + file);
+    });
+
+    const iconslist = filelist.map(file => `.icon--${file} { mask: url(../icons/${file}.svg); -webkit-mask: url(../icons/${file}.svg) }`);
+    if (!fs.existsSync(`../../public/css`)) {
+        fs.mkdirSync(`../../public/css`);
+    }
+
+    fs.writeFileSync(`../../public/css/icons.css`, `${iconslist.join(' ')} [class^=icon--],[class*=" icon--"]{width:24px;height:24px;display:block;-webkit-mask-size:cover;mask-size:cover;background-color:#000;-webkit-mask-repeat:no-repeat;mask-repeat:no-repeat;-webkit-mask-position:50% 50%;mask-position:50% 50%;}`);
+    console.log("Created icons")
+};
 
 const config = {
     entry: {
@@ -66,5 +106,7 @@ module.exports = () => {
     } else {
         config.mode = 'development';
     }
+    copyImages();
+    createIcons();
     return config;
 };
