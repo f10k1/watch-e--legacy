c4d087fee50f9e7149f7d77005a8db30aab9409e
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-03-02 13:22:09 +0100

    first reducer in react store

--- views/assets/react/store.ts
+++ views/assets/react/store.ts
@@ -1,7 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { AnyAction, combineReducers, configureStore, Store } from '@reduxjs/toolkit'
+import { Reducer } from 'react'
 import { useDispatch } from 'react-redux'
+
+interface stateType {
+    messages: any[]
+}
+
+const initialState: stateType = {
+    messages: []
+}
+
+const messagesReducer: Reducer<stateType, AnyAction> = (state = initialState, action: AnyAction) =>{
+    switch(action.type){
+        case 'messages/add':
+            return {...state, messages: action.id }
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({ messages: messagesReducer})
+
 const store = configureStore({
-    reducer: {}
+    reducer: rootReducer
 });
 
 export type RootState = ReturnType<typeof store.getState>
