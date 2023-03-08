cf3317dc9d79cf10f258a84808d4e59395fc710d
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-03-08 22:33:51 +0100

    user page

--- views/assets/react/Form/Input.tsx
+++ views/assets/react/Form/Input.tsx
@@ -1,9 +1,14 @@
-import React from "react"
-
-interface propsType{
+import React, { useState, forwardRef, useImperativeHandle, useEffect } from "react"
+import validators from '../../ts/helpers/validators'
+interface propsType {
     name: string,
     type: string,
+    value?: string,
+    ref?: any,
     label: string,
+    validation?: {
+        [key: string]: number | string | null
+    }
     messages?: {
         [key: string]: {
             status: boolean,
@@ -12,18 +17,53 @@ interface propsType{
     }
 }
 
+type InputHandle = {
+    validate: () => void
+}
+
+type state = {
+    value: string,
+    error: string
+}
+
 class Input extends React.Component<propsType>{
 
-    state: any = {
-        messages: []
+    state: state = {
+        value: this.props.value ?? '',
+        error: ''
     }
 
-    render(){
-        return <div className="form-row">
-            <input type={this.props.type} name={this.props.name} placeholder=" " id={this.props.name} />
-            <label htmlFor={this.props.name}>{this.props.label}</label>
-        </div> 
+    constructor(props: propsType) {
+        super(props)
+    }
+
+    validate = () => {
+        let validation = false
+        if (this.props.validation) {
+            Object.keys(this.props.validation).every((key: string) => {
+                if (typeof (validators[key] as any) === 'function') {
+                    const tmp: any = validators[key](this.state.value, this.props.validation && this.props.validation[key])
+                    this.setState(state => ({ ...state, error: tmp.msg ?? '' }))
+                    validation = tmp.status
+
+                    return tmp.status
+                }
+            })
+        }
+        return validation
+    }
+
+    render(): React.ReactNode {
+        return <div className={`form-row ${(this.state.error != '' || this.props.messages) && 'error'}`}>
+            <div className="input-helper">
+                <input type={this.props.type} name={this.props.name} placeholder=" " id={this.props.name} />
+                <label htmlFor={this.props.name}>{this.props.label}</label>
+            </div>
+            {this.state.error != '' && <span>{this.state.error}</span>}
+            {this.props.messages && Object.entries(this.props.messages).map((value) => <span key={value[0]}>{value[1].message}</span>)}
+        </div>
     }
 }
+
 
 export default Input
--- views/assets/react/Login.tsx
+++ views/assets/react/Login.tsx
@@ -1,44 +1,52 @@
-import React from "react";
+import React, { ElementRef, useRef, useState } from "react";
 import Input from './Form/Input'
-
-interface propsType{
+import axios from '../ts/helpers/axios'
+import t from '../ts/helpers/i18n'
+interface propsType {
+    lognUrl: string,
+    registerUrl: string,
     store: any
 }
 
-export default class Login extends React.Component<propsType>{
+type InputType = ElementRef<typeof Input>
 
-    store: any = this.props.store
+const Login = (props: propsType) => {
+    const store: any = props.store
 
-    state = {
-        page: 'Sign in'
-    }
+    const registerForm: React.RefObject<InputType[] | null> = useRef([])
 
-    constructor(props: propsType){
-        super(props)
+    const [login, setLogin] = useState(true)
+    const [messages, setMessages] = useState({})
 
-        this.store.subscribe(() => {
-            this.setState({
-                messages: this.store.getState().messages.messages
-            })
+    store.subscribe(() => {
+        setMessages({
+            messages: store.getState().messages.messages
         })
- 
+    })
+
+    const register = (event: React.FormEvent<HTMLElement>): void => {
+        event.preventDefault()
+        registerForm.current?.forEach((input: InputType) => {
+            console.log(input.validate())
+        })
     }
 
-    render(): React.ReactNode {
-        return <div className="container">
-            <div className="header">
-                <div className="logo">
-                    <span className="icon--camera-lens-fill"></span>
-                    Watch-E
-                </div>
-                <h1>{this.state.page}</h1>
+    return <div className="container">
+        <div className="header">
+            <div className="logo">
+                <span className="icon--camera-lens-fill"></span>
+                Watch-E
             </div>
-            <form action="/">
-                <Input name="login" type="text" label="Login" />
-                <Input name="email" type="email" label="E-mail" />
-                <Input name="password" type="password" label="Password" />
-                <Input name="password-repeat" type="password" label="Password repeat" />
-            </form>
+            <h1>{login ? t('Sign in') : t('Sign up')}</h1>
         </div>
-    }
+        <form onSubmit={register}>
+            <Input name={t('login')} type="text" label="Login" ref={(el: InputType) => registerForm.current?.push(el)} validation={{ required: null, min: 0 }} />
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Password" />
+            <Input name="password-repeat" type="password" label="Password repeat" />
+            <button type="submit" className="btn btn--red">{t('Submit')}</button>
+        </form>
+    </div>
 }
+
+export default Login
