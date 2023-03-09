4a20851762b736dedeaf2ae32cd4f193ca04a496
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-03-09 16:56:21 +0100

    axios integration for csrf

--- views/assets/ts/helpers/axios.js
+++ views/assets/ts/helpers/axios.js
@@ -1,7 +1,14 @@
 import axios from 'axios'
 
-const instance = axios.create({headers:{
-    'CSRF-Token': window.token
-}})
+const instance = axios.create({
+    headers: {
+        'Content-Type': 'multipart/form-data'
+    }, transformRequest: [(data) => {
+        return {
+            csrf: window.csrf ?? '', ...data
+        }
+    }, ...axios.defaults.transformRequest
+    ]
+})
 
 export default instance
