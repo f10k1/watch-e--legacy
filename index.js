ff90df6da7237f55a51467c6ade28a031b804d2e
Adrian Malec <67909049+f10k1@users.noreply.github.com>
2023-05-27 20:25:37 +0200

    (feature/backend) simple web socket server

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-const fs = require("fs")
+const fs = require("fs");
 const multer = require("multer");
-const passport = require("passport")
+const passport = require("passport");
 const route = require("./dist/routes");
 const dataSource = require("./dataSource");
 const i18next = require("./i18next.config");
@@ -12,7 +12,9 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const mysql2 = require('mysql2/promise');
 const MySQLStore = require('express-mysql-session');
-const { randomBytes } = require("crypto")
+const { randomBytes } = require("crypto");
+const ws = require("ws");
+const WsRoutes = require('./dist/routes').WsRoutes;
 
 dotenv.config({ path: path.resolve('./.env') });
 
@@ -33,17 +35,19 @@ const connection = mysql2.createPool({
 
 const sessionStore = MySQLStore({}, connection);
 
-app.use(session({
+const sessionParser = session({
     secret: process.env.SESSION_SECRET,
     name: 'session',
     store: sessionStore,
     resave: false,
     saveUninitialized: false,
     httpOnly: true,
-    sameSite: 'strict'
-}));
+    sameSite: 'lax'
+});
 
-app.use(passport.authenticate('session'))
+app.use(sessionParser);
+
+app.use(passport.authenticate('session'));
 
 app.use(i18nextHttpMiddleware.handle(i18next, {
     ignoreRoutes: ['/public', '/dist']
@@ -51,11 +55,11 @@ app.use(i18nextHttpMiddleware.handle(i18next, {
 
 global.t = i18next.t;
 
-const locales = {}
+const locales = {};
 
 fs.readdirSync('./locales').forEach(file => {
-    locales[file.replace('.json', '')] = JSON.parse(fs.readFileSync(`./locales/${file}`))
-})
+    locales[file.replace('.json', '')] = JSON.parse(fs.readFileSync(`./locales/${file}`));
+});
 
 app.use(express.static(path.resolve('./public')));
 
@@ -75,22 +79,22 @@ router.use((req, res, next) => {
     }
 
     if (res.locals.pageTitle === undefined) {
-        res.locals.pageTitle = 'Watch-e'
+        res.locals.pageTitle = 'Watch-e';
     }
 
-    if (res.locals.translations === undefined){
-        res.locals.translations = JSON.stringify(locales[req.language])
+    if (res.locals.translations === undefined) {
+        res.locals.translations = JSON.stringify(locales[req.language]);
     }
 
     if (req.session.csrf === undefined) {
-        req.session.csrf = randomBytes(100).toString('base64')
+        req.session.csrf = randomBytes(100).toString('base64');
     }
 
     if (res.locals.csrf === undefined) {
-        res.locals.csrf = req.session.csrf
+        res.locals.csrf = req.session.csrf;
     }
 
-    res.locals.user = req.user
+    res.locals.user = req.user;
 
     i18next.changeLanguage(req.session.lng);
     next();
@@ -102,4 +106,43 @@ route.default(router, upload, i18next.t);
 
 router.get('/', upload.none(), (req, res) => res.render("layouts/home", { style: 'home' }));
 
-app.listen(PORT);
+const UsersMap = new Map();
+
+const server = app.listen(PORT);
+
+const wsServer = new ws.Server({ noServer: true });
+
+server.on('upgrade', (request, socket, head) => {
+
+    socket.on('error', (err) => console.error(err));
+
+    sessionParser(request, {}, () => {
+
+        if (!request.session.passport?.user) {
+            socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
+            socket.destroy();
+            return;
+        }
+
+        wsServer.handleUpgrade(request, socket, head, socket => {
+            wsServer.emit('connection', socket, request);
+        });
+
+    });
+});
+
+wsServer.on('connection', (socket, req) => {
+    const user = req.session.passport.user;
+    const pathname = req.url;
+    let message;
+    UsersMap.set(user.id, socket);
+
+    if (!WsRoutes.get(pathname)) {
+        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+    }
+
+    socket.on('close', function () {
+        cleanup(user);
+        UsersMap.delete(user);
+    });
+});
